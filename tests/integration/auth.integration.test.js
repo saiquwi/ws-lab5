1b9130486091eb00858bd3ccbc5f21021c192df3
mayak <mayak@MAYA-COMP>
2025-11-26 15:15:01 +0300

    test: break integration test 2

--- tests/integration/auth.integration.test.js
+++ tests/integration/auth.integration.test.js
@@ -32,7 +32,7 @@ describe('Auth Integration Tests', () => {
   });
 
   describe('Public Routes', () => {
-    ('should access public content', async () => {
+    it(' access public content', async () => {
       const response = await request(app).get('/api/test/all');
       expect(response.status).toBe(200);
     });
